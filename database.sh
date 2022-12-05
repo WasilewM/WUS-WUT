580a58c1adedbd97adb2eafd243ac50c56703c8f
Bartłomiej Ściseł <bartlomiej.scisel.stud@pw.edu.pl>
2022-12-06 00:18:58 +0100

    Przepotężny commit.

--- database.sh
+++ database.sh
@@ -7,22 +7,22 @@ port=$3
 az vm run-command invoke --command-id RunShellScript \
                          --name $name \
                          --resource-group $resource_group \
-                         --scripts 'cd /root;
+                         --scripts "cd /root;
 sudo apt update;
 sudo apt upgrade -y;
 sudo apt install -y mysql-server;
 echo '[mysqld]' | sudo tee -a /etc/mysql/my.cnf;
-echo "port=3306" | sudo tee -a /etc/mysql/my.cnf;
-echo "bind-address = 0.0.0.0" | sudo tee -a /etc/mysql/my.cnf;
-echo "server-id = 1" | sudo tee -a /etc/mysql/my.cnf;
-echo "log_bin = /var/log/mysql/mysql-bin.log" | sudo tee -a /etc/mysql/my.cnf;
-echo "binlog_do_db = petclinic" | sudo tee -a /etc/mysql/my.cnf;
+echo 'port=3306' | sudo tee -a /etc/mysql/my.cnf;
+echo 'bind-address = 0.0.0.0' | sudo tee -a /etc/mysql/my.cnf;
+echo 'server-id = 1' | sudo tee -a /etc/mysql/my.cnf;
+echo 'log_bin = /var/log/mysql/mysql-bin.log' | sudo tee -a /etc/mysql/my.cnf;
+echo 'binlog_do_db = petclinic' | sudo tee -a /etc/mysql/my.cnf;
 
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS petclinic;
+sudo mysql -e '\"'CREATE DATABASE IF NOT EXISTS petclinic;
     ALTER DATABASE petclinic
       DEFAULT CHARACTER SET utf8
       DEFAULT COLLATE utf8_general_ci;
     CREATE USER IF NOT EXISTS 'pc'@'%' IDENTIFIED BY 'petclinic';
-    GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'%'";
+    GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'%''\"';
 
-sudo service mysql restart;'
+sudo service mysql restart;"
