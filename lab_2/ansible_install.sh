9d298a784af4bda92a77a8e3a195657e373d56ac
Mateusz Wasilewski <01158977@pw.edu.pl>
2022-12-29 13:54:22 +0100

    Updated deployment script.

--- lab_2/ansible_install.sh
+++ lab_2/ansible_install.sh
@@ -15,4 +15,27 @@ sudo apt install python3.9-distutils
 sudo apt install python3-pip
 python3.9 -m pip install --upgrade pip
 python3 -m pip install --user ansible
+# need to somehow remember the user value
 export PATH=/home/user/.local/bin:$PATH
+
+# need to capture wus-lab2-vm1 IP
+ssh user@wus-lab2-vm1_IP
+nano inventory.yaml
+nano id_rsa.pem
+chmod 600 id_rsa.pem
+ansible -i inventory.yaml all -m ping
+# expected answer:
+# host1 | SUCCESS => {
+#     "ansible_facts": {
+#         "discovered_interpreter_python": "/usr/bin/python3"
+#     },
+#     "changed": false,
+#     "ping": "pong"
+# }
+# host2 | SUCCESS => {
+#     "ansible_facts": {
+#         "discovered_interpreter_python": "/usr/bin/python3"
+#     },
+#     "changed": false,
+#     "ping": "pong"
+# }
