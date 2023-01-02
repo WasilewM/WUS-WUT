654e5d0e9208c1ce54b7df31199edfb084bad14e
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-01-02 22:13:53 +0100

    Updated scripts and config files for lab 2 config 1

--- lab_2/deploy_infractructure.sh
+++ lab_2/deploy_infractructure.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 az group create --location westeurope --name wus-lab2-rg
 
-az vm create -n wus-lab1-vm0-ansible -g wus-lab2-rg --image UbuntuLTS --generate-ssh-keys --public-ip-sku Standard
-az vm create -n wus-lab1-vm1-db-master -g wus-lab2-rg --image UbuntuLTS --generate-ssh-keys --public-ip-sku Standard
-az vm create -n wus-lab1-vm2-db-slave -g wus-lab2-rg --image UbuntuLTS --generate-ssh-keys --public-ip-sku Standard
-az vm create -n wus-lab1-vm3-backend -g wus-lab2-rg --image UbuntuLTS --generate-ssh-keys --public-ip-sku Standard
-az vm create -n wus-lab1-vm4-slave -g wus-lab2-rg --image UbuntuLTS --generate-ssh-keys --public-ip-sku Standard
+# WARNING: Limit: 3 public IPs 
+az vm create -n wus-lab1-vm0-ansible -g wus-lab2-rg --image Canonical:0001-com-ubuntu-server-jammy-daily:22_04-daily-lts-gen2:22.04.202211100 --generate-ssh-keys --public-ip-sku Standard
+az vm create -n wus-lab1-vm1-backend -g wus-lab2-rg --image Canonical:0001-com-ubuntu-server-jammy-daily:22_04-daily-lts-gen2:22.04.202211100 --generate-ssh-keys --public-ip-sku Standard
+az vm create -n wus-lab1-vm2-frontend -g wus-lab2-rg --image Canonical:0001-com-ubuntu-server-jammy-daily:22_04-daily-lts-gen2:22.04.202211100 --generate-ssh-keys --public-ip-sku Standard
--- lab_2/install_ansible.sh
+++ lab_2/install_ansible.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update 
 sudo apt upgrade
 sudo apt install software-properties-common
@@ -14,13 +15,13 @@ python3 -m pip install --user ansible
 # need to somehow remember the user value
 export PATH=/home/user/.local/bin:$PATH
 
-# need to capture wus-lab2-vm1 IP
-ssh user@wus-lab2-vm1_IP
-nano inventory.yaml
-nano id_rsa.pem
-chmod 600 id_rsa.pem
-ansible -i inventory.yaml all -m ping
-# expected answer:
+pip install ansible
+sudo apt install ansible-core
+sudo apt install ansible
+
+# chmod 600 id_rsa.pem
+# ansible -i inventory.yaml all -m ping
+# # expected answer:
 # host1 | SUCCESS => {
 #     "ansible_facts": {
 #         "discovered_interpreter_python": "/usr/bin/python3"
