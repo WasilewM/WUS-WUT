ef757e05c9e469289780accb1bf59703e8689e5b
Jan Rusak <jan.rusak.stud@pw.edu.pl>
2023-01-03 00:36:55 +0000

    Partially fixed ansible scripts

--- lab_2/ansible.sh
+++ lab_2/ansible.sh
@@ -2,6 +2,7 @@
 
 frontend_ip=$1
 backend_ip=$2
+user=$3
 
 cd /root
 sudo apt update -y
@@ -17,9 +18,8 @@ python3 -m pip show ansible
 sudo apt install ansible-core -y
 sudo apt install ansible -y
 
-cd ~/
-echo "frontend ansible_ssh_private_key_file=./id_rsa.pem ansible_user=$USER ansible_host=$frontend_ip" | sudo tee -a inventory.yaml
-echo "backend ansible_ssh_private_key_file=./id_rsa.pem ansible_user=$USER ansible_host=$backend_ip" | sudo tee -a inventory.yaml
+echo "frontend ansible_ssh_private_key_file=./id_rsa.pem ansible_user=$user ansible_host=$frontend_ip" | sudo tee -a inventory.yaml
+echo "backend ansible_ssh_private_key_file=./id_rsa.pem ansible_user=$user ansible_host=$backend_ip" | sudo tee -a inventory.yaml
 
 # copy id_rsa.pem before running this script
 
