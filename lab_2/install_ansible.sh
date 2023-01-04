aad127230230285277bda46538503211896aa827
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-01-04 12:56:01 +0100

    Updated README in lab 2 directory

--- lab_2/install_ansible.sh
+++ lab_2/install_ansible.sh
@@ -12,28 +12,3 @@ python3 -m pip show ansible
 sudo apt install ansible-core -y
 sudo apt install ansible -y
 ansible --version
-
-# Now manually create inventory file from provided template.
-#
-# Then copy the id_rsa.pem from your bash console to the vm0-ansible.
-#
-# Next, change permission for the key file with the following command
-# chmod 600 id_rsa.pem
-#
-# Finaly, you can check connection with the following command:
-# ansible -i inventory.yaml all -m ping
-# expected answer:
-# host1 | SUCCESS => {
-#     "ansible_facts": {
-#         "discovered_interpreter_python": "/usr/bin/python3"
-#     },
-#     "changed": false,
-#     "ping": "pong"
-# }
-# host2 | SUCCESS => {
-#     "ansible_facts": {
-#         "discovered_interpreter_python": "/usr/bin/python3"
-#     },
-#     "changed": false,
-#     "ping": "pong"
-# }
