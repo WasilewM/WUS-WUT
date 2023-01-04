4ccc1307aa57bce48641aa5664950b8312a6d8b4
mkowal21 <01158757@pw.edu.pl>
2023-01-04 01:26:42 +0100

    2 config

--- lab_2/scripts/database.sh
+++ lab_2/scripts/database.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 port=$1
 
-# echo "### BEFORE SCRIPT CLEANUP STARTED"
-# if [ -d ./database ]
-# then
-#     echo "Removed existing ./database directory"
-# 	sudo rm -rf ./database
-# fi
+echo "### BEFORE SCRIPT CLEANUP STARTED"
+if [ -d ./database ]
+then
+    echo "Removed existing ./database directory"
+	sudo rm -rf ./database
+fi
+
+echo "sudo mysql -e DROP USER pc;"
+sudo mysql -e "DROP USER pc;"
+
+echo "sudo mysql -e DROP DATABASE petclinic;"
+sudo mysql -e "DROP DATABASE petclinic;"
 
 if [ ! -d ./database ]
 then
@@ -27,13 +33,9 @@ echo "bind-address = 0.0.0.0" | sudo tee -a /etc/mysql/my.cnf
 echo "server-id = 1" | sudo tee -a /etc/mysql/my.cnf
 echo "log_bin = /var/log/mysql/mysql-bin.log" | sudo tee -a /etc/mysql/my.cnf
 
-sudo service restart mysql.service
-
-# sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql restart
 
 sudo mysql -e "CREATE USER IF NOT EXISTS 'pc'@'%' IDENTIFIED BY 'petclinic';"
-
-
 
 wget https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql
 sed -i "s/GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';/GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'%' WITH GRANT OPTION;/g" ./initDB.sql
