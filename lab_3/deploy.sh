50d12fd490bb23717a1fe9af0e5f1cb97d9a10b6
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-01-23 22:32:35 +0100

    Created deploy.sh script

---
+++ lab_3/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=$1
+CLUSTER_NAME=$2
+
+# create resource group
+az login
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
+
+# create aks cluster
+az aks create --resource-group $RESOURCE_GROUP_NAME \
+    --name $CLUSTER_NAME \
+    --enable-managed-identity \
+    --node-cout 2 \
+    --generate-ssh-keys
+
+kubectl get nodes
