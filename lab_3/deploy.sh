8f36c10349fa8dcd54e67518084fc66bfe1cd754
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-01-24 08:46:11 +0100

    Added aplications deployment to deploy.sh script

--- lab_3/deploy.sh
+++ lab_3/deploy.sh
@@ -15,3 +15,27 @@ az aks create --resource-group $RESOURCE_GROUP_NAME \
     --generate-ssh-keys
 
 kubectl get nodes
+
+# deploy spring-petclinic-cloud
+cd spring-petclinic-k8s/
+
+kubectl apply -f k8s/init-namespace
+
+kubectl apply -f k8s/init-services
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+helm search repo bitnami
+
+helm install vets-db-mysql bitnami/mysql --namespace spring-petclinic --version 9.4.6 --set auth.database=service_instance_db
+helm install visits-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.4.6 --set auth.database=service_instance_db
+helm install customers-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.4.6 --set auth.database=service_instance_db
+
+# spingcommunity repository on dockerhub:
+# https://hub.docker.com/u/springcommunity
+export REPOSITORY_PREFIX=springcommunity
+
+./scripts/deployToKubernetes.sh
+
+kubectl get svc -n spring-petclinic api-gateway
